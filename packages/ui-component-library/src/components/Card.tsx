e2ce439fb6cc1f560219040e33398734c8f8063a
Bernt Anker <bernt.anker@me.com>
2020-02-13 00:41:53 +0100

    Added Card Component

---
+++ packages/ui-component-library/src/components/Card.tsx
@@ -0,0 +1,36 @@
+import React, {  } from 'react'
+import '../style.css'
+
+export const Card = ({className, ...props}: {className: string}) => {
+  return (
+
+    <div className={`${className} max-w-sm rounded overflow-hidden shadow-lg`}>
+      <img
+        className="w-full"
+        src=""
+        alt="Sunset in the mountains"
+      />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+        <p className="text-gray-700 text-base">
+          Berntr Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+          quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
+          nihil.
+        </p>
+      </div>
+      <div className="px-6 py-4">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #photography
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #travel
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
+          #winter
+        </span>
+      </div>
+    </div>
+  )
+}
+
+
---
+++ packages/ui-component-library/src/components/LoginForm.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const LoginForm = () => {
+  return (
+    <h1 className="bg-gray-200">LOGIN FORM</h1>
+  )
+}
